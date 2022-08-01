bd5ddf58a4374eb1ee124a115ef6e730f51e56d6
KieSun <zx597813039@gmail.com>
2022-08-01 19:48:17 +0800

    feat: init dapp basic project

---
+++ dapp-basic/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
+}
+
+export default MyApp
---
+++ dapp-basic/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      Begin
+    </div>
+  )
+}
+
+export default Home
